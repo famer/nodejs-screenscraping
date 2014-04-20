e0bcac21020123b70c586d0961b9af5354fa9132
Тимур Татаршаов <famer@pksc5zaw.sin.cvut.cz>
2014-04-20 02:45:59 +0200

    express

--- web.js
+++ web.js
@@ -1,19 +1,14 @@
-var http = require('http');
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
 
-var express = require('express');
+app.use(logfmt.requestLogger());
 
-var router = express();
-var server = http.createServer(router);
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
-router.use(express.static(path.resolve(__dirname, 'client')));
-
-router.get('/', function(req, res) { 
-    res.send("Asdf");
-    });
-    
-
-
-server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-  var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
